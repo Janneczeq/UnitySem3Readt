73226b3937b36b9c5228cea2fbafddbb74f3c2bb
Janneczeq <janzagajewski007@gmail.com>
2021-11-29 19:52:55 +0100

    29.11.2021
    BAZA + photon

--- Assets/Scripts/RaceLauncher.cs
+++ Assets/Scripts/RaceLauncher.cs
@@ -3,15 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using Photon.Realtime;
+using Photon.Pun;
 
-public class RaceLauncher : MonoBehaviour
+
+public class RaceLauncher : MonoBehaviourPunCallbacks
 {
-    public InputField playerName;
 
-    void Start()
+
+
+    public InputField playerName;
+    byte maxPlayerPerRoom = 3;
+    bool isConnecting;
+    public Text networkText;
+    string gameVersion = "2";
+
+  void Awake()
     {
+        PhotonNetwork.AutomaticallySyncScene = true;
         if (PlayerPrefs.HasKey("PlayerName")) playerName.text = PlayerPrefs.GetString("PlayerName");
+     
     }
+    
+  
+       
 
     public void SetName(string name)
     {
@@ -21,5 +36,20 @@ public class RaceLauncher : MonoBehaviour
     public void StartTrial()
     {
         SceneManager.LoadScene(0);
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        if (isConnecting)
+        {
+            networkText.text += "OnConnectToMaster.. \n";
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        networkText.text += "Failed to join random room. \n";
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = this.maxPlayerPerRoom });
+        
     }
 }
